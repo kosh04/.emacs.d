f17fa3bbb78faae85c7a1162bc66035836bb0e70
KOBAYASHI Shigeru <shigeru.kb@gmail.com>
2015-05-12 13:55:29 +0900

    enable frame transparent on osx

--- config/cocoa-emacs-conf.el
+++ config/cocoa-emacs-conf.el
@@ -5,3 +5,7 @@
 ;; Font
 (set-face-attribute 'default nil :family "Menlo" :height 150)
 ;;(set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Hiragino Kaku Gothic ProN"))
+
+;;(add-to-list 'default-frame-alist '(alpha . (0.75 0.75)))
+(set-frame-parameter nil 'alpha 90)
+
