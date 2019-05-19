e9ac5e3539f0fbccafb29318d372c69bc891576a
kosh04 <shigeru.kb@gmail.com>
2019-05-19 22:01:50 +0900

    Add my emacs-lisp submodules

--- config/50-golang.el
+++ config/50-golang.el
@@ -44,7 +44,7 @@
 ;; go-playground client
 ;; https://github.com/kosh04/emacs-go-playground
 (use-package go-playground-cli
-  :load-path "~/Documents/GitHub/emacs-go-playground"
+  :load-path "~/.emacs.d/site-lisp/go-playground-cli"
   :after go-mode
   :bind (:map go-mode-map
               ("C-c w w" . go-playground-cli-run-current-file)))
