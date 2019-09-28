0eb6ebd3e90eed3ab473e9cf7e8adc8cbf9ccca2
kosh04 <shigeru.kb@gmail.com>
2019-09-29 00:44:29 +0900

    Show caption into mode-line

--- config/50-orgmode.el
+++ config/50-orgmode.el
@@ -3,3 +3,9 @@
 (add-hook 'org-mode-hook 'outline-show-all)
 (add-hook 'org-mode-hook 'which-func-mode)
 ;;(add-hook 'org-mode-hook 'view-mode)
+
+(defun org-mode/which-func-functions ()
+    (setq-local which-func-functions
+                (lambda ()
+                  (string-join (org-get-outline-path t) " > "))))
+(add-hook 'org-mode-hook 'org-mode/which-func-functions)
