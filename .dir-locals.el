75a4b2fbfb69e19e84c214591aad17007ef52c39
kosh04 <shigeru.kb@gmail.com>
2019-10-02 14:59:22 +0900

    Sample .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil
+  ;; M-x bug-reference-prog-mode
+  (bug-reference-bug-regexp . "\\(?:issue\\|pull\\) ?#\\(?2:[0-9]+\\)")
+  (bug-reference-url-format . "https://github.com/kosh04/.emacs.d/issues/%s")))
