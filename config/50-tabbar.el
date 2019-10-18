d1c96639a2cff7f73977c4c11c11a987fc7389f5
kosh04 <shigeru.kb@gmail.com>
2019-10-19 04:31:57 +0900

    Modify tab-bar settings

--- config/50-tabbar.el
+++ config/50-tabbar.el
@@ -1,33 +1,5 @@
 ;;; config/tabbar
 
-;; new feature tabs (emacs-27+)
-(use-package tab-bar
-  :ensure nil
-  :if (version<= "27.0" emacs-version)
-  :init (tab-bar-mode +1)
-  :bind (("C-x 6 l" . tab-bar-list)
-         ("C-x 6 b" . tab-bar-select-tab)
-         ;;("C-x 6 o" . tab-bar-switch-to-next-tab)
-         ;;("C-x 6 p" . tab-bar-switch-to-prev-tab)
-         ("C-x C-." . tab-next)
-         ("C-x C-," . tab-previous))
-  :config
-  (set-face-attribute 'tab-bar nil :height 1.0)
-  ;;(set-face-attribute 'tab-bar-tab nil :inherit 'header-line)
-  (set-face-attribute 'tab-bar-tab nil :inherit 'success)
-  (set-face-attribute 'tab-bar-tab-inactive nil :foreground (face-foreground 'default) :background (face-background 'default) :inverse-video t)
-  )
-
-(use-package tab-line
-  :ensure nil
-  :if (version<= "27.0" emacs-version)
-  :init (global-tab-line-mode +1)
-  :config
-  (set-face-attribute 'tab-line-tab nil :inherit 'warning)
-  ;;(set-face-attribute 'tab-line-tab nil :background "#00F" :foreground (face-background 'highlight))
-  (set-face-attribute 'tab-line-tab-inactive nil :foreground (face-foreground 'default) :background (face-background 'default) :inverse-video t)
-  )
-
 (use-package tabbar
   :if (not (version<= "27.0" emacs-version))
   :hook
@@ -76,4 +48,26 @@
                       :background (face-attribute 'mode-line :background)
                       :foreground (face-attribute 'mode-line :foreground)
                       :box '(:line-width -1 :style released-button))
+  )
+
+;; new feature tabs (Emacs 27+)
+(use-package tab-bar
+  :ensure nil
+  :if (version<= "27.0" emacs-version)
+  :init (tab-bar-mode +1)
+  :custom
+  (tab-bar-tab-hints t)
+  :bind (("C-x 6 ." . tab-next)
+         ("C-x 6 ," . tab-previous)
+         ("C-x 6 l" . tab-bar-list))
+  :config
+  (tab-rename "*main*")
+  (require 'hi-lock)
+  (set-face-attribute 'tab-bar-tab nil :inherit 'hi-green)
+  )
+
+(use-package tab-line
+  :ensure nil
+  :if (version<= "27.0" emacs-version)
+  ;;:init (global-tab-line-mode +1)
   )
