b8204a2a407e1363d63dabe81a8da7feffe54f5d
kosh04 <shigeru.kb@gmail.com>
2020-05-31 14:39:37 +0900

    Update org

--- config/50-orgmode.el
+++ config/50-orgmode.el
@@ -1,4 +1,4 @@
-;;; config/Org
+;;; config/Org-mode
 
 (use-package org
   :hook
@@ -11,3 +11,13 @@
                   (string-join (org-get-outline-path t) " > "))))
   (add-hook 'org-mode-hook 'org-mode/which-func-functions)
   )
+
+(with-eval-after-load 'find-file
+  ;; [init.el 読書会用] config.org <-> config.el を行来する
+  (add-to-list 'cc-other-file-alist '("\\.org\\'" (".el")))
+  (add-to-list 'cc-other-file-alist '("\\.el\\'" (".org"))))
+
+(defun dired-tangle (filename)
+  "[user] Dired 内で *.org -> *.el の生成."
+  (interactive (list (dired-get-filename)))
+  (org-babel-tangle-file filename))
