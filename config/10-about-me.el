a3234d299658ea707c1faadac6e74625f7128aea
kosh04 <shigeru.kb@gmail.com>
2020-06-16 01:20:48 +0900

    avoid custom-set-variables

--- config/10-about-me.el
+++ config/10-about-me.el
@@ -1,6 +1,7 @@
 ;;; config/about-me
 
-(custom-set-variables
- '(user-full-name "KOBAYASHI Shigeru")
- '(user-login-name "kosh") ;; or "kosh04"
- '(user-mail-address "shigeru.kb@gmail.com"))
+(use-package emacs
+  :custom
+  (user-full-name "KOBAYASHI Shigeru")
+  (user-login-name "kosh") ;; or "kosh04"
+  (user-mail-address "shigeru.kb@gmail.com"))
